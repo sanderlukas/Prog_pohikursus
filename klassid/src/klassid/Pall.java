8055db0d2ff1dafefbd9f7a6b70f56a665b44127
if17 <if17@A406-17.INFORMAATIKA>
2018-02-08 10:26:15 +0200

    2. nädal - klassid

---
+++ klassid/src/klassid/Pall.java
@@ -0,0 +1,48 @@
+package klassid;
+
+public class Pall {
+    protected double x;
+    protected double y;
+    protected double r;
+    
+    public Pall(double x, double y, double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    
+    public Pall(double x, double y) {
+        /*this.x = x;
+        this.y = y;*/
+        this(x, y, 1);
+    }
+    
+    public double kaugusNullist() {
+        return Math.sqrt(x*x + y*y);
+    }
+    
+    public double getX() { return x;}
+    public double getY() {return y;}
+    public double getR() {return r;}
+    
+    public double kaugusPallist(Pall teine) {
+        double dx = getX() - teine.getX();
+        double dy = getY() - teine.getY();
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+    
+    public double pallRaadius(double x, double y) {
+        double keskk_x = getX();
+        double keskk_y = getY();
+        return Math.sqrt(Math.pow((x - keskk_x), 2) + Math.pow((y - keskk_y), 2));
+    }
+    
+    public boolean kasPuutub(Pall teine) {
+        return kaugusPallist(teine) < getR() + teine.getR();
+    }
+    
+    @Override
+    public String toString() {
+        return "Pall kohal (" + x + ", " + y + ")";
+    }
+}
---
+++ klassid/src/klassid/Proov.java
@@ -0,0 +1,17 @@
+package klassid;
+public class Proov {
+    public static void main(String[] args) {
+        Pall p1 = new Pall(3, 5);
+        Pall p2 = new Pall(4, 6);
+        System.out.println(p1 + " Kaugus algkohast: " + p1.kaugusNullist());
+        System.out.println(p2);
+        System.out.println("Omavaheline kaugus: " + p1.kaugusPallist(p2));
+        System.out.println("Palli raadius: " + p1.pallRaadius(5, 3));
+        
+        if (p1.kasPuutub(p2)) {
+            System.out.println("1 ja 2 puutuvad");
+        }
+        
+    }
+    
+}
